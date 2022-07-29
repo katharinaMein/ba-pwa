e5ec26aa0230055eb07159a6f410b6f70bf457d9
DESKTOP-EJO1RF7\katha <katharina@rseibert.de>
2022-07-29 12:58:30 +0200

    gps functionality

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.less']
 })
 export class LocationComponent implements OnInit {
+  options: {timeout: number, maximumAge: number, enableHighAccuracy: boolean} = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  longitude?: number;
+  latitude?: number;
+  accuracy?: number;
+
+  buttonClicked: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    if('geolocation' in navigator) {
+      console.log('Geolocation available');
+    } else {
+      console.log('Geolocation not available');
+    }
   }
 
+  onGetCoordinates(){
+    navigator.geolocation.getCurrentPosition((position:GeolocationPosition): void => {
+      this.success(position)
+    },
+      positionError => this.error(positionError),
+      this.options);
+    this.buttonClicked = true;
+  }
+
+  success(pos: GeolocationPosition) {
+    const crd = pos.coords;
+    this.longitude = crd.longitude;
+    this.latitude = crd.latitude;
+    this.accuracy = crd.accuracy;
+  }
+
+  error(err: GeolocationPositionError) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
 }
