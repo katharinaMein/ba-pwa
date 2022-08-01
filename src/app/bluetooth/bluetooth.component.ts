24b6b5ce400d5a018d7cb5c81b859082fc794c39
DESKTOP-EJO1RF7\katha <katharina@rseibert.de>
2022-08-01 22:10:53 +0200

    wip

--- src/app/bluetooth/bluetooth.component.ts
+++ src/app/bluetooth/bluetooth.component.ts
@@ -1,5 +1,5 @@
 /// <reference types="web-bluetooth" />
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-bluetooth',
@@ -8,15 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BluetoothComponent implements OnInit {
   options = {
-    acceptAllDevices: true
+    acceptAllDevices: true,
+    optionalServices: ['battery_service']
   }
 
   bluetoothAvailable!: boolean;
+  battery?: number | null;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
-    if(navigator.bluetooth){
+    if (navigator.bluetooth) {
       this.bluetoothAvailable = true;
     } else {
       this.bluetoothAvailable = false;
@@ -24,34 +27,48 @@ export class BluetoothComponent implements OnInit {
   }
 
   onRequestDevice() {
-    if (this.bluetoothAvailable){
-      navigator.bluetooth.requestDevice(this.options).then(
-        device => {
-          console.log(device.name);
-          return device.gatt?.connect();
-        }
-      )
-        .then((server: BluetoothRemoteGATTServer | undefined) => {
-          return server!.getPrimaryService('battery_service');
-        }
-      )
-        .then(service =>{
-          return service.getCharacteristic('battery_level');
-        }
-      )
-        .then(characteristics =>{
-          return characteristics.readValue();
-        }
-      )
-        .then(value =>{
-          console.log('The battery level of the connected device is' + value);
+    if (this.bluetoothAvailable) {
+      navigator.bluetooth.requestDevice(this.options).then(device => device.gatt?.connect())
+        .then(async (server: BluetoothRemoteGATTServer | undefined) => {
+            return server?.getPrimaryService('battery_service');
           }
         )
-        .catch( error => {
-        console.log(error);
-      })
-    } else{
+        .then(service => {
+            return service?.getCharacteristic('battery_level');
+          }
+        )
+        .then(characteristics => {
+            return characteristics?.readValue();
+          }
+        )
+        .then((value) => {
+            console.log('The battery level of the connected device is: ' + value?.getInt8(0) + '%');
+          }
+        )
+        .catch(error => {
+          console.log(error);
+        })
+    } else {
       console.log('Can´t do this, because Bluetooth is not available :(')
     }
+  }
+
+  async onRequestDevice2() {
+    if (this.bluetoothAvailable) {
+      const device = await navigator.bluetooth.requestDevice(this.options);
+      const server = await device.gatt?.connect();
+      const service = await server!.getPrimaryService('battery_service');
+      const characteristics = await service.getCharacteristic('battery_level');
+      const buffer = await characteristics?.readValue();
+      const batteryLevel = buffer.getInt8(0);
+
+      return batteryLevel;
+    } else {
+      return null;
+    }
+  }
+
+  getBattery() {
+    this.onRequestDevice2().then(val => this.battery = val);
   }
 }
