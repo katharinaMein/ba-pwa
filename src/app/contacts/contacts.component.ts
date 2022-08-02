f271d48b9dfad1bbdbda3c3c5cfd2f6445e2ffbe
DESKTOP-EJO1RF7\katha <katharina@rseibert.de>
2022-08-02 17:44:01 +0200

    contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.less']
 })
 export class ContactsComponent implements OnInit {
+  contacts!: [{name: string, tel: string}];
+  contactsAvailable: boolean = true;
+  contactProperties = ['name', 'tel'];
+  options = {multiple: true};
 
   constructor() { }
 
   ngOnInit(): void {
+    if(!('contacts' in navigator && 'ContactsManager' in window)){
+      this.contactsAvailable = false;
+    }
+  }
+
+  onGetContacts(){
+    this.contacts = (navigator as any).contacts.select(this.contactProperties, this.options);
+    debugger;
   }
 
 }
