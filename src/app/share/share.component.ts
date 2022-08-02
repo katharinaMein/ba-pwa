fadc9a4cdd708ea81da28dd0c65759d016c9d1a9
DESKTOP-EJO1RF7\katha <katharina@rseibert.de>
2022-08-02 11:10:36 +0200

    share

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share.component.less']
 })
 export class ShareComponent implements OnInit {
+  shareData: ShareData = {
+    text: ''
+  };
+
+  errorCaught: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  async onShareText(shareInput: string){
+    this.shareData.text = shareInput;
+    try{
+      await navigator.share(this.shareData);
+    }
+    catch(error){
+      this.errorCaught = true;
+      console.log(error);
+    }
+  }
 }
